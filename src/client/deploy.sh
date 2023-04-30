631cb2cf003145e6400b555ed011979e08371916
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-04-30 18:01:37 +0530

    Minor Changes

--- src/client/deploy.sh
+++ src/client/deploy.sh
@@ -3,7 +3,13 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+echo "Deploying client..."
+echo "Building client..."
 npm run build
+echo "Removing old files..."
 rm -rf /var/www/html/*
+echo "Copying new files..."
 cp -r dist/* /var/www/html/
+echo "Setting permissions..."
 chown -R www-data:www-data /var/www/html
+echo "Done!"
