8bff0f6753f4d13ad13cda7ab615fd8a75b68536
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-04-30 19:34:32 +0530

    Update

--- src/client/deploy.sh
+++ src/client/deploy.sh
@@ -11,10 +11,16 @@ read API_URL
 echo "VITE_API_URL=$API_URL" > .env
 echo "Building client..."
 npm run build
+if ! [ -x "$(command -v nginx)" ]; then
+  echo "Installing nginx..."
+  apt-get install -y nginx
+fi
 echo "Removing old files..."
 rm -rf /var/www/html/*
 echo "Copying new files..."
 cp -r dist/* /var/www/html/
 echo "Setting permissions..."
 chown -R www-data:www-data /var/www/html
+echo "Restarting nginx..."
+systemctl restart nginx
 echo "Done!"
